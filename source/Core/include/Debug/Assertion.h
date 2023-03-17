8b3132312829cf0a3239f7c1a1035cf7f3d7445f
Loick Noa Obiang Ndong <l.obiangndong@student.isartdigital.com>
2023-03-16 21:08:33 -0400

    Added ASSERT macro

--- source/Core/include/Debug/Assertion.h
+++ source/Core/include/Debug/Assertion.h
@@ -1,6 +1,14 @@
 #pragma once
 
-namespace My::Debug
-{
+#ifndef ASSERT
+#ifdef _DEBUG
 
-}
+#define ASSERT(condition) if (!(condition)) __debugbreak()
+
+#else
+
+#include <cstdlib>
+#define ASSERT(condition) if (!(condition)) abort()
+
+#endif // _DEBUG
+#endif // !ASSERT
