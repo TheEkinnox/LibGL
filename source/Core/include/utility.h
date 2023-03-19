06f4096cbe0e5d90bce99afeb0559eb297ad1b33
Loick Noa Obiang Ndong <l.obiangndong@student.isartdigital.com>
2023-03-19 02:31:05 -0400

    Added string splitting function

---
+++ source/Core/include/utility.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#include <vector>
+#include <string>
+
+namespace My
+{
+	/**
+	 * \brief Splits the given string using the given delimiter
+	 * \param str The string to split
+	 * \param delimiter The delimiter to use to split the string
+	 * \param includeEmpty Whether empty substrings should be included in the result
+	 * \return A vector containing the sub-strings
+	 */
+	std::vector<std::string> splitString(const std::string& str, char const* delimiter, bool includeEmpty);
+}
