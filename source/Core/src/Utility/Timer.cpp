fad94cf733520ba1536a86b484c12fa3d52ac82b
Loick Noa Obiang Ndong <l.obiangndong@student.isartdigital.com>
2023-03-21 18:33:29 -0400

    Added Timer class

---
+++ source/Core/src/Utility/Timer.cpp
@@ -0,0 +1,52 @@
+#include "Utility/Timer.h"
+
+#include <chrono>
+
+namespace My::Utility
+{
+	Timer::Timer(const float timeScale)
+		: m_timeScale(timeScale)
+	{
+	}
+
+	void Timer::update()
+	{
+
+		m_lastUpdate = m_currentTime;
+		m_currentTime = clock::now();
+		m_deltaTime = std::chrono::duration<float>(m_currentTime - m_lastUpdate).count();
+
+		m_unscaledTime += m_deltaTime;
+		m_time += m_deltaTime * m_timeScale;
+	}
+
+	float Timer::getTime() const
+	{
+		return m_time;
+	}
+
+	float Timer::getDeltaTime() const
+	{
+		return m_deltaTime * m_timeScale;
+	}
+
+	float Timer::getUnscaledTime() const
+	{
+		return m_unscaledTime;
+	}
+
+	float Timer::getUnscaledDeltaTime() const
+	{
+		return m_deltaTime;
+	}
+
+	float Timer::getTimeScale() const
+	{
+		return m_timeScale;
+	}
+
+	void Timer::setTimeScale(const float timeScale)
+	{
+		m_timeScale = timeScale;
+	}
+}
