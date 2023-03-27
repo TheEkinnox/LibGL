c61ce7aa68e069ab6dd6987d7a4de443e96a3b04
Loick Noa Obiang Ndong <l.obiangndong@student.isartdigital.com>
2023-03-27 01:31:39 -0400

    Fixed mesh copy constructor not copying transform

--- source/LowRenderer/include/Mesh.h
+++ source/LowRenderer/include/Mesh.h
@@ -14,12 +14,12 @@ namespace My::Rendering
 
 						Mesh() = delete;
 		explicit		Mesh(const Model& model);
-						Mesh(const Mesh& other);
-						Mesh(Mesh&& other) noexcept;
+						Mesh(const Mesh& other) = default;
+						Mesh(Mesh&& other) noexcept = default;
 						~Mesh() = default;
 
-		Mesh&			operator=(const Mesh& other);
-		Mesh&			operator=(Mesh&& other) noexcept;
+		Mesh&			operator=(const Mesh& other) = default;
+		Mesh&			operator=(Mesh&& other) noexcept = default;
 
 		void			setModel(const Model& model);
 
--- source/LowRenderer/src/Mesh.cpp
+++ source/LowRenderer/src/Mesh.cpp
@@ -7,41 +7,6 @@ namespace My::Rendering
 	{
 	}
 
-	Mesh::Mesh(const Mesh& other)
-		: m_model(other.m_model)
-	{
-	}
-
-	Mesh::Mesh(Mesh&& other) noexcept
-		: m_transform(std::move(other.m_transform)), m_model(other.m_model)
-	{
-		other.m_model = nullptr;
-	}
-
-	Mesh& Mesh::operator=(const Mesh& other)
-	{
-		if (&other == this)
-			return *this;
-
-		m_transform = other.m_transform;
-		m_model = other.m_model;
-
-		return *this;
-	}
-
-	Mesh& Mesh::operator=(Mesh&& other) noexcept
-	{
-		if (&other == this)
-			return *this;
-
-		m_transform = std::move(other.m_transform);
-		m_model = other.m_model;
-
-		other.m_model = nullptr;
-
-		return *this;
-	}
-
 	void Mesh::setModel(const Model& model)
 	{
 		m_model = &model;
