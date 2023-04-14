1c296238ef955a28d92b9fa0e38869b225726ea6
Loick Noa Obiang Ndong <l.obiangndong@student.isartdigital.com>
2023-04-14 02:43:24 -0400

    Added Entity default constructor

--- source/Entities/include/Entity.h
+++ source/Entities/include/Entity.h
@@ -14,6 +14,7 @@ namespace LibGL
 		typedef std::vector<ComponentPtr> ComponentList;
 
 	public:
+		Entity() = default;
 		Entity(Entity* parent, const Transform& transform);
 
 		template <typename T, typename ... Args>
