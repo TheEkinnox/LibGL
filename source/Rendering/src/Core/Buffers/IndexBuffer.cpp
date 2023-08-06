3174a1a5e748121133abdbb5937db86d61036eaa
Loick Noa Obiang Ndong <l.obiangndong@student.isartdigital.com>
2023-08-06 04:50:32 -0400

    Fixed index buffer ctor generating a vertex buffer

--- source/Rendering/src/Core/Buffers/IndexBuffer.cpp
+++ source/Rendering/src/Core/Buffers/IndexBuffer.cpp
@@ -1,5 +1,4 @@
 #include "Core/Buffers/IndexBuffer.h"
-#include "Resources/Vertex.h"
 
 #include <glad/glad.h>
 
@@ -8,20 +7,14 @@ namespace LibGL::Rendering
 	IndexBuffer::IndexBuffer(const uint32_t* indices, const intptr_t indexCount)
 	{
 		glGenBuffers(1, &m_bufferIndex);
-		glBindBuffer(GL_ARRAY_BUFFER, m_bufferIndex);
-		glBufferData(GL_ARRAY_BUFFER, indexCount * static_cast<GLsizeiptr>(sizeof(Resources::Vertex)),
+		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, m_bufferIndex);
+		glBufferData(GL_ELEMENT_ARRAY_BUFFER, indexCount * static_cast<GLsizeiptr>(sizeof(uint32_t)),
 			indices, GL_STATIC_DRAW);
 	}
 
-	IndexBuffer::IndexBuffer(const std::vector<uint32_t>& indices)
+	IndexBuffer::IndexBuffer(const std::vector<uint32_t>& indices) :
+		IndexBuffer(indices.data(), static_cast<intptr_t>(indices.size()))
 	{
-		const uint32_t* idsArray = indices.data();
-
-		glGenBuffers(1, &m_bufferIndex);
-		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, m_bufferIndex);
-		glBufferData(GL_ELEMENT_ARRAY_BUFFER,
-			static_cast<GLsizeiptr>(indices.size()) * static_cast<GLsizeiptr>(sizeof(uint32_t)),
-			idsArray, GL_STATIC_DRAW);
 	}
 
 	void IndexBuffer::bind() const
