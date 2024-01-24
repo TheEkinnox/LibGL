78e197127df46b3640a42935b2b352d3bc417668
Loick Noa Obiang Ndong <l.obiangndong@student.isartdigital.com>
2024-01-23 19:06:51 -0500

    Added trim string utility functions

--- source/Core/include/Utility/utility.h
+++ source/Core/include/Utility/utility.h
@@ -1,36 +1,57 @@
 #pragma once
 
+#include <functional>
 #include <vector>
 #include <string>
 
 namespace LibGL::Utility
 {
+    /**
+     * \brief Formats a string using the printf syntax
+     * \tparam Args The arguments to insert into the format string
+     * \param format The format string (follows printf syntax)
+     * \param args The arguments to insert into the format string
+     * \return The formatted string
+     */
+    template <class... Args>
+    std::string formatString(const char* format, Args... args);
 
-	/**
-	 * \brief Formats a string using the printf syntax
-	 * \tparam Args The arguments to insert into the format string
-	 * \param format The format string (follows printf syntax)
-	 * \param args The arguments to insert into the format string
-	 * \return The formatted string
-	 */
-	template	<class ... Args>
-	std::string formatString(const char* format, Args ... args);
+    /**
+     * \brief Splits the given string using the given delimiter
+     * \param str The string to split
+     * \param delimiter The delimiter to use to split the string
+     * \param includeEmpty Whether empty substrings should be included in the result
+     * \return A vector containing the sub-strings
+     */
+    std::vector<std::string> splitString(const std::string& str, const std::string& delimiter, bool includeEmpty);
 
-	/**
-	 * \brief Splits the given string using the given delimiter
-	 * \param str The string to split
-	 * \param delimiter The delimiter to use to split the string
-	 * \param includeEmpty Whether empty substrings should be included in the result
-	 * \return A vector containing the sub-strings
-	 */
-	std::vector<std::string> splitString(const std::string& str, const std::string& delimiter, bool includeEmpty);
+    /**
+     * \brief Trims the beginning of the given string using the given compare func
+     * \param str The string to trim
+     * \param compareFunc The function used to check if a character should be trimmed
+     */
+    inline void trimStringStart(std::string& str, const std::function<bool(char)>& compareFunc = &isspace);
 
-	/**
-	 * \brief Reads the lines of the given text file
-	 * \param fileName The file's path
-	 * \return A vector containing the file's lines
-	 */
-	std::vector<std::string> readFile(const std::string& fileName);
+    /**
+     * \brief Trims the end of the given string using the given compare func
+     * \param str The string to trim
+     * \param compareFunc The function used to check if a character should be trimmed
+     */
+    inline void trimStringEnd(std::string& str, const std::function<bool(char)>& compareFunc = &isspace);
+
+    /**
+     * \brief Trims the given string using the given compare func
+     * \param str The string to trim
+     * \param compareFunc The function used to check if a character should be trimmed
+     */
+    inline void trimString(std::string& str, const std::function<bool(char)>& compareFunc = &isspace);
+
+    /**
+     * \brief Reads the lines of the given text file
+     * \param fileName The file's path
+     * \return A vector containing the file's lines
+     */
+    std::vector<std::string> readFile(const std::string& fileName);
 }
 
 #include "Utility/utility.inl"
--- source/Core/include/Utility/utility.inl
+++ source/Core/include/Utility/utility.inl
@@ -1,38 +1,54 @@
 #pragma once
 
 #include "Utility/utility.h"
+
+#include <algorithm>
 #include <stdexcept>
 
 namespace LibGL::Utility
 {
-	template<typename ... Args>
-	std::string formatString(const char* const format, Args ... args)
-	{
-		// If no template parameters are passed
-		// return the format string as is to avoid unnecessary allocation
-		if constexpr (sizeof...(Args) == 0)
-		{
-			return format;
-		}
-		else
-		{
-			// get the formatted text's size
-			const int intSize = std::snprintf(nullptr, 0, format, args...) + 1;
+    template <typename... Args>
+    std::string formatString(const char* const format, Args... args)
+    {
+        // If no template parameters are passed
+        // return the format string as is to avoid unnecessary allocation
+        if constexpr (sizeof...(Args) == 0)
+            return format;
+        else
+        {
+            // get the formatted text's size
+            const int intSize = std::snprintf(nullptr, 0, format, args...) + 1;
 
-			if (intSize <= 0)
-				throw std::runtime_error("Unable to format string.");
+            if (intSize <= 0)
+                throw std::runtime_error("Unable to format string.");
 
-			// Create a buffer of the computed size
-			const size_t bufferSize = intSize;
-			char* const buffer = static_cast<char*>(alloca(bufferSize * sizeof(char)));
+            // Create a buffer of the computed size
+            const size_t bufferSize = intSize;
+            char* const  buffer = static_cast<char*>(alloca(bufferSize * sizeof(char)));
 
-			// Write the formatted string in the buffer
-			std::snprintf(buffer, bufferSize, format, args...);
+            // Write the formatted string in the buffer
+            std::snprintf(buffer, bufferSize, format, args...);
 
-			// Copy the buffer data into an std::string
-			std::string message(buffer, buffer + bufferSize - 1);
+            // Copy the buffer data into an std::string
+            std::string message(buffer, buffer + bufferSize - 1);
 
-			return message;
-		}
-	}
+            return message;
+        }
+    }
+
+    inline void trimStringStart(std::string& str, const std::function<bool(char)>& compareFunc)
+    {
+        str.erase(str.begin(), std::ranges::find_if_not(str, compareFunc));
+    }
+
+    inline void trimStringEnd(std::string& str, const std::function<bool(char)>& compareFunc)
+    {
+        str.erase(std::find_if_not(str.rbegin(), str.rend(), compareFunc).base(), str.end());
+    }
+
+    inline void trimString(std::string& str, const std::function<bool(char)>& compareFunc)
+    {
+        trimStringEnd(str, compareFunc);
+        trimStringStart(str, compareFunc);
+    }
 }
